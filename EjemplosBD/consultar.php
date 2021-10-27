670ac2d6ad488f6b217940e9063726a051e866a5
SrAlexjajaXD <alexloga18@gmail.com>
2021-10-27 13:36:27 -0500

    Correcion de errores respecto a la base de datos

--- EjemplosBD/consultar.php
+++ EjemplosBD/consultar.php
@@ -1,10 +1,11 @@
 <?php
 $pass = "lkpoaszxm2001";
 $usuario = "postgres";
-$nombreBaseDeDatos = "NewBase";
+$nombreBaseDeDatos = "newbase";
 # Puede ser 127.0.0.1 o el nombre de tu equipo; o la IP de un servidor remoto
 $rutaServidor = "127.0.0.1";
 $puerto = "5432";//si fuera MySQL el puerto seria 3306
+echo "HOLA<br>";
 try {
     $base_de_datos = new PDO("pgsql:host=$rutaServidor;port=$puerto;dbname=$nombreBaseDeDatos", $usuario, $pass);
     $base_de_datos->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);//para capturar errores
